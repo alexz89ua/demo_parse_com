20c60eae259584c42ffbc3bb98f02f07e5801f1e
alexz89ua <alexz89ua@gmail.com>
2014-10-31 14:27:29 +0200

    add screens

--- app/src/main/java/com/stfalcon/demoparse/Screen2.java
+++ app/src/main/java/com/stfalcon/demoparse/Screen2.java
@@ -7,39 +7,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.parse.Parse;
-import com.parse.ParseAnalytics;
 
-import java.util.HashMap;
-import java.util.Map;
-
-
-public class MainActivity extends Activity implements View.OnClickListener{
+public class Screen2 extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Parse.initialize(this, "y2cC0qr4NB4lyhXTNH6rAOfjXdSDTcRlVDn9CBWX", "cwIFp05Wn2hhtNwE4KdNudjZvXVzHZBLabRephHE");
-        ParseAnalytics.trackAppOpened(getIntent());
-
-        Map<String, String> dimensions = new HashMap<String, String>();
-        // What type of news is this?
-        dimensions.put("category", "politics");
-        // Is it a weekday or the weekend?
-        dimensions.put("dayType", "weekday");
-        // Send the dimensions to Parse along with the 'read' event
-
-        ParseAnalytics.trackEvent("read", dimensions);
-
-        findViewById(R.id.bt_screen2).setOnClickListener(this);
+        setContentView(R.layout.activity_screen2);
+        findViewById(R.id.bt_screen3).setOnClickListener(this);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_screen2, menu);
         return true;
     }
 
@@ -61,8 +43,8 @@ public class MainActivity extends Activity implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.bt_screen2:
-                startActivity(new Intent(MainActivity.this, Screen2.class));
+            case R.id.bt_screen3:
+                startActivity(new Intent(Screen2.this, Screen3.class));
                 break;
         }
     }
